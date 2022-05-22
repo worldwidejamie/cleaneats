5a5cfd9b167232fedf714561aaf2dd57a1d4a49b
Jamie Smith <hello@worldwidejamie.com>
2022-05-22 12:31:45 -0500

    Added status icons
    Visual indicators of clean or dirty restaurants

--- src/components/PassingRestaurantGrid.jsx
+++ src/components/PassingRestaurantGrid.jsx
@@ -11,60 +11,51 @@ import Typography from "@mui/material/Typography";
 import GppGoodIcon from "@mui/icons-material/GppGood";
 
 export default function PassingRestaurantGrid() {
-	return (
-	<Container sx={{mt: 4,}}>
-		<Typography variant="h4" component="h2" sx={{px: 2}}>
-			These eats are verified clean!
-			</Typography>
-			<Box sx={{flexGrow: 1, mt: 2, px: 2}}>
-		<Grid container spacing={2}>
-			<Grid item sm={6} md={4}>
-				<Card>
-					<CardHeader
-						title="Delicious Foods"
-						subheader="Passing"
-					/>
-					<CardContent>
-						<CardMedia
-							component="img"
-							image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
-							alt="Beard Papa's restaurant facade"
-						/>
-					</CardContent>
-				</Card>
-			</Grid>
-			<Grid item sm={6} md={4}>
-				<Card>
-					<CardHeader
-						title="Delicious Foods"
-						subheader="Passing"
-					/>
-					<CardContent>
-						<CardMedia
-							component="img"
-							image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
-							alt="Beard Papa's restaurant facade"
-						/>
-					</CardContent>
-				</Card>
-			</Grid>
-			<Grid item sm={6} md={4}>
-				<Card>
-					<CardHeader
-						title="Delicious Foods"
-						subheader="Passing"
-					/>
-					<CardContent>
-						<CardMedia
-							component="img"
-							image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
-							alt="Beard Papa's restaurant facade"
-						/>
-					</CardContent>
-				</Card>
-			</Grid>
-		</Grid>
-	</Box>
-	</Container>
-	)
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" component="h2" sx={{ px: 2 }}>
+        These eats are verified clean! <GppGoodIcon htmlColor="green" />
+      </Typography>
+      <Box sx={{ flexGrow: 1, mt: 2, px: 2 }}>
+        <Grid container spacing={2}>
+          <Grid item sm={6} md={4}>
+            <Card>
+              <CardHeader title="Delicious Foods" subheader="Passing" />
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
+                  alt="Beard Papa's restaurant facade"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item sm={6} md={4}>
+            <Card>
+              <CardHeader title="Delicious Foods" subheader="Passing" />
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
+                  alt="Beard Papa's restaurant facade"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item sm={6} md={4}>
+            <Card>
+              <CardHeader title="Delicious Foods" subheader="Passing" />
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image="https://images.squarespace-cdn.com/content/v1/600a96cbf8894d79eb1e1b1f/1611317333085-PBPKUT3S28T0CV0IJUL8/_34A7895-min.jpg?format=1500w"
+                  alt="Beard Papa's restaurant facade"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
