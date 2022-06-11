b8aff307e321ea48382809a3599685fb2fc656b5
Jamie Smith <hello@worldwidejamie.com>
2022-06-11 10:13:00 -0500

    format file

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { CssBaseline } from '@mui/material';
-import App from './App'
-import AppTopBar from './components/AppBar'
-import PassingRestaurantGrid from './components/PassingRestaurantGrid'
-import FailingRestaurantGrid from './components/FailingRestaurantGrid';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+import App from "./App";
+import AppTopBar from "./components/AppBar";
+import PassingRestaurantGrid from "./components/PassingRestaurantGrid";
+import FailingRestaurantGrid from "./components/FailingRestaurantGrid";
 
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
     <AppTopBar />
-    <PassingRestaurantGrid />
     <FailingRestaurantGrid />
+    <PassingRestaurantGrid />
   </React.StrictMode>
-)
+);
