56c41100dffabce1eb9f15428fcba5d39d249ba4
Jamie Smith <hello@worldwidejamie.com>
2022-08-03 09:33:26 -0500

    Code formatting

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
-import App from "./App";
 import AppTopBar from "./components/AppBar";
 import RestaurantGrid from "./components/RestaurantGrid";
 
 import "./index.css";
+// const document = document;
 
-if( typeof document !== 'undefined') {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-        <React.StrictMode>
-            <CssBaseline/>
-            <AppTopBar/>
-            <RestaurantGrid inspectionResult="Pass"/>
-            <RestaurantGrid inspectionResult="Fail"/>
-        </React.StrictMode>
-    );
+if (typeof document !== "undefined") {
+  // eslint-disable-next-line no-undef
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <CssBaseline />
+      <AppTopBar />
+      <RestaurantGrid inspectionResult="Pass" />
+      <RestaurantGrid inspectionResult="Fail" />
+    </React.StrictMode>
+  );
 }
