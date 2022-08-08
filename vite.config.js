1d366be6564dc54631a4151ff8b6db3434625c76
Jamie Smith <hello@worldwidejamie.com>
2022-08-08 11:01:07 -0500

    Added proxy to get around CORS issues

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true,
+    proxy: {
+      '/api': {
+        target: 'https://maps.googleapis.com',
+        changeOrigin: true,
+        ws: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   },
   base: "/cleaneats/",
   plugins: [
     react()
-  ]
+  ],
+
 })
